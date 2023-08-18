6495d52243172c33c191daae1029c71b9b5fa9ce
tandras92 <47755290+tandras92@users.noreply.github.com>
2023-08-17 20:37:25 -0500

    fix test

--- src/test/java/com/tandras/ld/HealthCareLdApplicationTests.java
+++ src/test/java/com/tandras/ld/HealthCareLdApplicationTests.java
@@ -1,9 +1,10 @@
 package com.tandras.ld;
 
+import com.tandras.ld.healthcare.HealthCareLdApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HealthCareLdApplication.class)
 class HealthCareLdApplicationTests {
 
 	@Test
