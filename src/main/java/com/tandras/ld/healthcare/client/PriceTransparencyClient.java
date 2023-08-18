8920f2776138a9cc7dcfacc37b65e0f3acc61650
tandras92 <47755290+tandras92@users.noreply.github.com>
2023-08-17 20:42:24 -0500

    move encode/decode logic to the service

--- src/main/java/com/tandras/ld/healthcare/client/PriceTransparencyClient.java
+++ src/main/java/com/tandras/ld/healthcare/client/PriceTransparencyClient.java
@@ -1,6 +1,7 @@
 package com.tandras.ld.healthcare.client;
 
 import com.tandras.ld.healthcare.dto.HospitalRepositoryMetadata;
+import com.tandras.ld.healthcare.service.PriceTransparencyService;
 import com.tandras.ld.healthcare.util.UrlUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -8,33 +9,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class PriceTransparencyClient {
 
     private final WebClient webClient;
-    private static final String DEFAULT_QUERY =
-            """
-            SELECT *
-            FROM `hospital`
-            ORDER BY `id` ASC
-            LIMIT 200""";
+
+    private final PriceTransparencyService priceTransparencyService;
 
     public Mono<HospitalRepositoryMetadata> getHospitals(String query) {
-        Optional<String> optionalQuery = Optional.ofNullable(query);
-        String encodedSqlQuery;
-        if (optionalQuery.isPresent()) {
-            encodedSqlQuery = UrlUtil.encodeUrl(optionalQuery.get());
-        } else {
-            encodedSqlQuery = UrlUtil.encodeUrl(DEFAULT_QUERY);
-        }
 
+        String encodedSqlQuery = priceTransparencyService.processQuery(query);
         return webClient.get()
-                .uri(UrlUtil.decodeUrl("https://www.dolthub.com/api/v1alpha1/dolthub/transparency-in-pricing/main?q=" + encodedSqlQuery))
+                .uri(UrlUtil.decodeUrl("/transparency-in-pricing/main?q=" + encodedSqlQuery))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(HospitalRepositoryMetadata.class);
